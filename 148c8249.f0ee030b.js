0b2c4e3fa3fde720f30bb231d6df3e5a11d0b488
Benoît Rognier <benoit.rognier@edulera.com>
2020-12-15 14:26:15 +0100

    Deploy website - based on d31751f32d439f1b4db05719f154c5a5ad4ef32a

---
+++ 148c8249.f0ee030b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{68:function(o){o.exports=JSON.parse('{"permalink":"/completium-landing/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ e2c602e6.e4295e05.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{94:function(o){o.exports=JSON.parse('{"allTagsPath":"/completium-landing/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/completium-landing/blog/tags/facebook"}')}}]);
