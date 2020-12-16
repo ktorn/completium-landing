cdb78873030980bf4def38d01e272d90cb62705a
Benoît Rognier <benoit.rognier@edulera.com>
2020-12-16 10:51:33 +0100

    adds team section

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,19 @@ import Typography from '@material-ui/core/Typography';
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import Divider from '@material-ui/core/Divider';
 import useWindowDimensions from '../utils/WindowDimension';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
+import PhoneIcon from '@material-ui/icons/Phone';
+import { Parallax } from "react-parallax";
 
 function Banner() {
   const { width: width } = useWindowDimensions();
   return (
     <Grid container direction='column' justify="center" alignItems="center" style={{
       height: '380px',
-      backgroundImage: "linear-gradient(rgba(0 0 78 / 34%), rgba(178 189 77 / 41%)), url("+useBaseUrl('img/banner2.svg')+")",
+      backgroundImage: "linear-gradient(rgba(0 0 78 / 34%), rgba(178 189 77 / 41%))",
+/*       backgroundImage: "linear-gradient(rgba(0 0 78 / 34%), rgba(178 189 77 / 41%)), url("+useBaseUrl('img/banner2.svg')+")",
+ */
       backgroundSize: width+'px',
       backgroundRepeat: 'repeat-x'
       }}>
@@ -41,7 +47,7 @@ function Banner() {
         <Typography variant='h5' style={{ fontFamily : 'Alegreya Sans SC, sans-serif' }}>From conception to completion</Typography>
       </Grid>
       <Grid item style={{textAlign: 'center', marginTop: '100px'}}>
-        <Typography>We help you create <span style={{ fontWeight: 'bold'}}>Dapps</span> from off-chain application (front&back-ends)<br/> to verified smart contracts</Typography>
+        <Typography>We create <span style={{ fontWeight: 'bold'}}>Dapps</span> from off-chain application (front&back-ends)<br/> to verified smart contracts</Typography>
       </Grid>
     </Grid>
   )
@@ -105,7 +111,7 @@ function LearnDapps() {
           </Grid>
           <Grid item xs={1}></Grid>
           <Grid item xs={2} style={{ textAlign: 'center' }}>
-            <Feature name='undraw_setup_wizard' title='Step by Step Tutorials' text='Follow step by step instructions to implement, deploy and interact with smart contracts, connect the Dapp to wallet, ...'/>
+            <Feature name='undraw_setup_wizard' title='Step by Step Tutorials' text='Follow step by step instructions to implement, deploy and interact with smart contracts, connect the Dapp to a wallet, ...'/>
           </Grid>
           <Grid item xs={1}></Grid>
         </Grid>
@@ -151,15 +157,109 @@ function Archetype() {
   )
 }
 
+function Avatar(props) {
+  return (
+    <div style={{ display: 'grid', alignItems: 'baselinecenter', justifyContent: 'center', textAlign: 'center' }}>
+      <img src={useBaseUrl('img/'+props.portrait+'.jpg')} style={{
+        width: '100px',
+        borderRadius: '100%',
+        justifySelf: 'center',
+        marginBottom: '20px',
+        border: 'px solid #4a4a4a' }}></img>
+      <Typography style={{ fontWeight: 'bold' }}>{props.name}
+        <Link target="_blank" rel="noopener" href={"https://www.linkedin.com/in/"+props.url+"/"} underline='none'>
+          <LinkedInIcon style={{ verticalAlign: 'bottom', marginLeft: '10px'}}/>
+        </Link>
+      </Typography>
+      <Typography>{props.title}</Typography>
+    </div>
+  )
+}
+
 function Service() {
   return (
-    <Grid container direction='column' justify="center" alignItems="center" style={{ padding: '50px' }}>
+    <Grid container direction='column' justify="center" alignItems="center" style={{ padding: '50px' }} spacing={4}>
       <Grid item xs={12} style={{ textAlign: 'center' }}>
         <Typography variant='h4' style={{ fontWeight: 'bold' }}>
-          Nice to meet you! <EmojiEmotionsIcon />
+          Nice to meet you! <EmojiEmotionsIcon fontSize="large" style={{  verticalAlign: 'bottom' }}/>
         </Typography>
       </Grid>
+      <Grid item>
+        <Grid container direction='row' justify="center" alignItems="center" style={{ paddingTop: '50px', paddingBottom: '50px' }}>
+          <Grid item xs={2}>
+            <Typography align='justify'>Benoit and Guillaume created edukera  in 2013 for the passion of sharing and teaching science and technology. In 2017 they became enthousiastic for the blockchain technology and in 2018 started to develop the Archetype language with the support of the Tezos Foundation. </Typography>
+          </Grid>
+          <Grid item xs={2} >
+            <Avatar portrait='br' name='Benoît Rognier' title='CEO edukera' url="benoitrognier"/>
+          </Grid>
+          <Grid item xs={2}>
+            <Avatar portrait='gd' name='Guillaule Duhamel' title='CTO edukera' url="guillaumeduhamel"/>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Typography variant='h6' style={{ fontWeight: 'bold' }}>Can we help you achieve your objective?</Typography>
+      </Grid>
+      <Grid item style={{ width: '70%' }}>
+        <Grid container direction='row' justify="center" alignItems="center">
+          <Grid item>
+            <EmailIcon style={{ marginRight: '20px', verticalAlign: 'bottom' }}/>
+          </Grid>
+          <Grid item>
+            <Typography>contact@edukera.com</Typography>
+          </Grid>
+          <Grid item xs={2} />
+          <Grid item>
+            <PhoneIcon style={{ marginRight: '20px', verticalAlign: 'bottom' }}/>
+          </Grid>
+          <Grid item>
+            <Typography>+33 6 18 40 30 62</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Typography variant='h4' style={{ fontWeight: 'bold', marginTop: '70px' }}>Advisory board</Typography>
+      </Grid>
+      <Grid item>
+        <Grid container direction='row' justify="center" alignItems="center" spacing={6} style={{ marginTop: '10px' }}>
+          <Grid item>
+            <Avatar portrait='dofp' name='Diego Olivier Fernandez Pons' title='Business Advisor' url="diego-olivier-fernandez-pons"/>
+          </Grid>
+          <Grid item>
+            <Avatar portrait='pys' name='Pierre-Yves Strub' title='Scientific Advisor' url="pierre-yves-strub-9822506"/>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
+  )
+}
+
+function Scrollable() {
+  const thiselem = React.createRef();
+  console.log(`ref: ${thiselem}`);
+  const { height } = useWindowDimensions();
+  return (
+    <div id="scrollable" style={{ height: height, overflow: 'scroll' }} >
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={12}>
+          <Parallax bgImage={useBaseUrl('img/banner2.svg')} strength={500} parent={document.getElementById("scrollable")}>
+            <Banner />
+          </Parallax>
+        </Grid>
+        <Grid item xs={12}><Divider></Divider></Grid>
+        <Grid item xs={12}>
+          <LearnDapps />
+        </Grid>
+        <Grid item xs={12}><Divider></Divider></Grid>
+        <Grid item xs={12}>
+          <Archetype />
+        </Grid>
+        <Grid item xs={12}><Divider></Divider></Grid>
+        <Grid item xs={12}>
+          <Service />
+        </Grid>
+      </Grid>
+      </div>
   )
 }
 
@@ -177,29 +277,10 @@ function Home() {
       }),
     [prefersDarkMode],
   );
-  const { height } = useWindowDimensions();
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div style={{ height: height, overflow: 'scroll' }} >
-      <Grid container direction="row" justify="center" alignItems="center">
-        <Grid item xs={12}>
-          <Banner />
-        </Grid>
-        <Grid item xs={12}><Divider></Divider></Grid>
-        <Grid item xs={12}>
-          <LearnDapps />
-        </Grid>
-        <Grid item xs={12}><Divider></Divider></Grid>
-        <Grid item xs={12}>
-          <Archetype />
-        </Grid>
-        <Grid item xs={12}><Divider></Divider></Grid>
-        <Grid item xs={12}>
-          <Service />
-        </Grid>
-      </Grid>
-      </div>
+      <Scrollable />
     </ThemeProvider>
   );
 }
