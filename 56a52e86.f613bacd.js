6732609a9a08fd3b7e208b1d426e989248fc6ea2
Benoît Rognier <benoit.rognier@edulera.com>
2020-12-17 18:47:50 +0100

    Deploy website - based on 88c21d4010a715f017bc07cc4c4e4048a1fd4c08

---
+++ 56a52e86.f613bacd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{79:function(l){l.exports=JSON.parse('{"allTagsPath":"/completium-landing/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/completium-landing/blog/tags/hello"}')}}]);
--- 93979c17.52bf3bcf.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{84:function(l){l.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/completium-landing/blog/welcome"},{"title":"Hello","permalink":"/completium-landing/blog/hello-world"},{"title":"Hola","permalink":"/completium-landing/blog/hola"}]}')}}]);
