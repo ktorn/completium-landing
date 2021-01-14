26bc05f2ed9f0169c8fbe3868837b34d17698343
Benoît Rognier <benoit.rognier@edulera.com>
2021-01-14 11:08:58 +0100

    Deploy website - based on 3d9d8a577a3ff986caf53db9f7cbf189f4f6c1d3

--- 56a52e86.6d4df2fe.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{78:function(l){l.exports=JSON.parse('{"allTagsPath":"/completium-landing/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/completium-landing/blog/tags/hello"}')}}]);
