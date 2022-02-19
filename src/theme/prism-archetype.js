c3f29c83bad02b7d009b2229a039e7ba3bd02e4a
Benoit Rognier <benoit.rognier@edukera.com>
2022-02-19 10:50:33 +0100

    fix highlighting

--- src/theme/prism-archetype.js
+++ src/theme/prism-archetype.js
@@ -24,10 +24,10 @@
 		// For the list of keywords and operators,
 		// see: http://caml.inria.fr/pub/docs/manual-ocaml/lex.html#sec84
 		'declaration': /\b(?:constant|archetype|enum|states|variable|asset|entry|transition|function|getter|var|let some|specification|postcondition|record|invariant)\b/,
-		'keyword': /\b(?:as|require|failif|called|by|shadow|effect|initial|identified by|initialized by|assert|begin|end|do|done|else|balance|otherwise|exec_lambda|return|before|for|if|in|match|in|forall|added|removed|exists|now|struct|then|caller|transfer|from|to|while|with|when|fails|operations|InvalidCondition|InvalidCaller|NotFound|NatAssign|SliceError|DivByZero|OutOfBound)\b/,
+		'keyword': /\b(?:as|require|failif|called|by|shadow|effect|initial|identified by|initialized by|assert|begin|end|do|done|else|balance|otherwise|exec_lambda|return|before|for|if|in|match|in|forall|added|removed|exists|now|struct|then|caller|transferred|transfer|from|to|while|with|when|fails|operations|InvalidCondition|InvalidCaller|NotFound|NatAssign|SliceError|DivByZero|OutOfBound)\b/,
 		'boolean': /\b(?:false|true)\b/,
 		// Custom operators are allowed
-		'operator': /:=|\+=|\-=|[=<>@^|&+\-*\/$%!?~][!$%&*+\-.\/:<=>?@^|~]*|\b(?:and|not|asr|land|lor|lsl|lsr|lxor|mod|or|remove|removeif|count|update|nth|add|put|addupdate|dofailif|dorequire|sum|fail|some|isempty|length|transferred|selfaddress|the|state|min|max|contains)\b/,
+		'operator': /:=|\+=|\-=|[=<>@^|&+\-*\/$%!?~][!$%&*+\-.\/:<=>?@^|~]*|\b(?:and|not|asr|land|lor|lsl|lsr|lxor|mod|or|remove|removeif|count|update|nth|add|put|addupdate|dofailif|dorequire|sum|fail|some|isempty|length|selfaddress|the|state|min|max|contains)\b/,
 		'punctuation': /[(){}\[\]|.,:;]|\b_\b/
 	};
 }(Prism));
